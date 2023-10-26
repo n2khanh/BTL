84981491670be79b51483174268939833fbecde8
n2khanh <n2.khanh.git@gmail.com>
2023-10-26 14:31:31 +0700

    update

--- js/datve.js
+++ js/datve.js
@@ -18,14 +18,15 @@ function togglePasswordVisibility() {
     var email = document.getElementById("email").value;
     var numberOfTickets = document.getElementById("numberOfTickets").value;
     var password = document.getElementById("password").value;
+    var chair=document.getElementById("chair").value;
   
     // Regular expression for email validation.
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   
-    if (fullname.trim() === "" || !email.match(emailPattern) || numberOfTickets < 1 || password.trim() === "") {
+    if (fullname.trim() === "" || !email.match(emailPattern) || numberOfTickets < 1 || password.trim() === ""|| chair.trim() === "" ) {
       alert("Please fill in all the required fields with valid data.");
     } else {
       // Redirect to another page (change "newpage.html" to the desired URL).
-      window.location.href = "../BTL/Confiming.html";
+      window.location.href = "../confirm/Confiming.html";
     }
   }
